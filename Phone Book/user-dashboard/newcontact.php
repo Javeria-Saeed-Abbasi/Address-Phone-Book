d0e026772baf4e48ad176aec7b514642849f6547
Javeria-Saaed-Abbasi <jiyasaeed333@gmail.com>
2020-09-05 03:14:56 +0500

    admin DASHBOARD is in process

---
+++ Phone Book/user-dashboard/newcontact.php
@@ -0,0 +1,87 @@
+<?php
+require('../db/config.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <!-- Font Awsome cdn link -->
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <!-- Custom Stylesheet -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+	<title>Add New Contact</title>
+	<style>
+		#edit-contact label {
+	color: #3488fc;
+	font-weight:bold;
+  }
+		</style>
+</head>
+<body>
+	<?php include('includes/navbar.php'); ?>
+
+
+		<!-- ================ Content Div ==================== -->
+                    
+<div class="card" >
+			<div class="card-header">
+				<!-- <strong >Contact of First Name</strong> -->
+				<a href="index.php" class="float-right btn btn-dark btn-sm">
+				<i class="fa fa-fw fa-globe"></i>Contact List</a>
+			 </div>
+			<div class="card-body ">
+				
+				<div class=" col-6 col-sm-6 col-md-6 mx-auto" id="edit-contact">
+					<!-- <h5 class="card-title">Fields with <span class="text-danger">*</span> are mandatory!</h5> -->
+					<form method="post">
+						<div class="form-group ">
+							<label>First Name <span class="text-danger">*</span></label>
+							<input type="text" name="firstname" id="firstname" class="form-control" value="<?php echo $row[0]['username']; ?>" placeholder="Enter first name" required>
+						</div>
+						<div class="form-group ">
+							<label>Last Name <span class="text-danger">*</span></label>
+							<input type="text" name="lastname" id="lastname" class="form-control" value="<?php echo $row[0]['username']; ?>" placeholder="Enter last name" required>
+						</div>
+						<div class="form-group">
+							<label>Email <span class="text-danger">*</span></label>
+							<input type="email" name="email" id="email" class="form-control" value="<?php echo $row[0]['useremail']; ?>" placeholder="Enter email" required>
+						</div>
+						<div class="form-group">
+							<label>Phone <span class="text-danger">*</span></label>
+							<input type="tel" name="userphone" id="userphone" maxlength="12" class="form-control" value="<?php echo $row[0]['userphone']; ?>" placeholder="Enter phone" required>
+						</div>
+						<div class="form-group">
+							<label for="inputAddress">Address</label>
+							<input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+						</div>
+						<div class="form-group">
+							<label for="inputCity">City</label>
+							<input type="text" class="form-control" id="inputCity" placeholder="Karachi">
+						</div>
+						<div class="form-group ">
+							<label for="inputCountry">Country</label>
+							<input type="text" class="form-control" id="inputCountry" placeholder="Pakistan">
+						</div>
+						
+					
+						<div class="form-group">
+							<input type="hidden" name="editId" id="editId" value="<?php echo $_REQUEST['editId']?>">
+							<button type="submit" name="submit" value="submit" id="submit" class="btn btn-primary"><i class="fa fa-fw fa-plus"></i>Add Contact</button>
+                            <br/><br/><br/>
+							<center><a href="index.php">Back to Contact List</a><center>
+                        </div>
+						
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- ==================== FOOTER ============================ -->
+	<?php include('includes/footer.php'); ?>
+</body>
+</html>
